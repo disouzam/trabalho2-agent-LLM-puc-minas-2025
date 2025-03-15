37a42b1e842676972fb562af3adbfe1c9f61e0c0
Dickson Souza <36424026+disouzam@users.noreply.github.com>
2025-03-15 11:48:26 -0300

    refactor: Propriedades da classe ChatResponse ajustadas para usar padrão de nomenclatura do C# e indicar explicitamente conversão de nomes de propriedades recebidas da API da OpenAI
    - Referência: https://learn.microsoft.com/en-us/dotnet/standard/serialization/system-text-json/customize-properties#use-a-built-in-naming-policy

--- src/Trabalho1_IA/Chat/ChatResponse.cs
+++ src/Trabalho1_IA/Chat/ChatResponse.cs
@@ -1,15 +1,32 @@
-﻿using ProcessoChat.LLM;
+﻿using System.Text.Json.Serialization;
+
+using ProcessoChat.LLM;
 
 namespace ProcessoChat.Chat;
 
 public class ChatResponse
 {
-    public string id { get; set; }
-    public string @object { get; set; }
-    public int created { get; set; }
-    public string model { get; set; }
-    public List<ChoiceResponse> choices { get; set; }
-    public UsageResponse usage { get; set; }
-    public string service_tier { get; set; }
-    public string system_fingerprint { get; set; }
+    [JsonPropertyName("id")]
+    public string Id { get; set; }
+
+    [JsonPropertyName("object")]
+    public string Object { get; set; }
+
+    [JsonPropertyName("created")]
+    public int Created { get; set; }
+
+    [JsonPropertyName("model")]
+    public string Model { get; set; }
+
+    [JsonPropertyName("choices")]
+    public List<ChoiceResponse> Choices { get; set; }
+
+    [JsonPropertyName("usage")]
+    public UsageResponse Usage { get; set; }
+
+    [JsonPropertyName("service_tier")]
+    public string ServiceTier { get; set; }
+
+    [JsonPropertyName("system_fingerprint")]
+    public string SystemFingerprint { get; set; }
 }
