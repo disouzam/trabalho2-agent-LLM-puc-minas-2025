66cb94b2fe2cc0e60b757c766f333d64caee49b3
Deivisson <deivisson.tecnico@gmail.com>
2025-03-16 10:08:55 -0300

    style: jogar o metodos para nova classe

--- src/Trabalho1_IA/Program.cs
+++ src/Trabalho1_IA/Program.cs
@@ -1,9 +1,4 @@
-﻿using System.Text;
-using System.Text.Json;
-
-
-using ProcessoChat.Chat;
-using ProcessoChat.LLM;
+﻿using ProcessoChat.LLM;
 using ProcessoChat.Servicos;
 
 namespace ProcessoChat;
@@ -91,7 +86,9 @@ public class Program
             string contexto = string.Join("\n", chunksTotal);
             string promptFinal = $"Contexto relevante:\n{contexto}\n\nPergunta:\n{pergunta}";
 
-            var resposta = await EnviarParaOpenAI(promptFinal, MaxTokensResposta);
+            var resposta = await OpenAIClient.EnviarParaOpenAI(promptFinal, MaxTokensResposta);
+
+            ContextoAtualizado = resposta.Item2.Any(x => x.contextoAtualizado == true);
 
             var textoDaResposta = resposta.Item1;
             if(textoDaResposta.Contains("Não sei", StringComparison.OrdinalIgnoreCase))
@@ -121,88 +118,4 @@ public class Program
 
         Console.WriteLine("Chat encerrado.");
     }
-
-    private static async Task<(string, List<UsageResponse>)> EnviarParaOpenAI(string prompt, int maxTokensResposta)
-    {
-        using var client = new ClientAPI().ObterClientAPI();
-        const string modelName = "gpt-4o-mini";
-
-        var usageStatistics = new List<UsageResponse>();
-
-        var result = await ChamadaPrincipal(client, modelName, prompt, maxTokensResposta);
-
-        usageStatistics.Add(result.Usage);
-
-        if(result.Choices.First().Message.FunctionCall != null)
-        {
-            string functionName = result.Choices.First().Message.FunctionCall.Name;
-            string argumentsJson = result.Choices.First().Message.FunctionCall.Arguments;
-
-            if(functionName == "GetProcessoExterno")
-            {
-                var result2 = await ProcessoExternoService.ObterInfoProcessoExterno(client, modelName, argumentsJson);
-                ContextoAtualizado = true;
-
-                usageStatistics.Add(result.Usage);
-                var resposta2 = result2.Choices.First().Message.Content;
-                return (resposta2, usageStatistics);
-            }
-        }
-
-        var resposta = result.Choices.First().Message.Content;
-        return (resposta, usageStatistics);
-    }
-
-    private static async Task<ChatResponse> ChamadaPrincipal(HttpClient client, string modelName, string prompt, int maxTokensResposta)
-    {
-        var payload = new
-        {
-            model = modelName,
-
-            messages = new[]
-            {
-                new
-                {
-                    role = "system",
-                    content = @"Você deve responder **exclusivamente** com base no contexto fornecido. Se a resposta não estiver no contexto,
-                    também pode usar a função para buscar, caso não encontrar em nenhum responda  'Não sei'."
-                },
-                new
-                {
-                    role = "user",
-                    content = prompt
-                }
-            },
-            functions = new[]
-            {
-                new
-                {
-                    name = "GetProcessoExterno",
-                    description = "Busca detalhes de um processo fora do modelo de dados usando uma api externa",
-                    parameters = new
-                    {
-                        type = "object",
-                        properties = new
-                        {
-                            Numero = new { type = "integer", description = "Numero do processo" }
-                        },
-                        required = new[] { "Numero" }
-                    }
-                }
-            },
-
-            temperature = 0.4,
-            max_tokens = maxTokensResposta,
-        };
-
-        string jsonPayload = JsonSerializer.Serialize(payload);
-
-        var response = await client.PostAsync(ClientAPI.OpenAiEndpoint,
-            new StringContent(jsonPayload, Encoding.UTF8, "application/json"));
-
-        string responseText = await response.Content.ReadAsStringAsync();
-        var result = JsonSerializer.Deserialize<ChatResponse>(responseText);
-        return result;
-    }
-
 }
